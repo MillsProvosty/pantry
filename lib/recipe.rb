70e7840bf2fd3dded6f733b29dd435cfeca853be
Mills Provosty <millsprovosty@gmail.com>
2019-02-26 10:17:48 -0700

    Added ingredients test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -7,6 +7,17 @@ class Recipe
   end
 
   def add_ingredient(ingredient, quantity)
-  @ingredients_required[ingredient] = quantity
+    @ingredients_required[ingredient] = quantity
   end
+
+  def amount_required(ingredient_object)
+    @ingredients_required.map do |object, quantity|
+      if object == ingredient_object
+        return quantity
+      end
+    end
+
+  end
+
+
 end
